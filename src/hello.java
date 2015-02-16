a0ca2ea66d70fae73e0af736039d2f893479036e
egoing <shining0206@naver.com>
2015-02-17 00:26:05 +0900

    new commit

--- src/hello.java
+++ src/hello.java
@@ -1,26 +1,44 @@
-class Hello
-{
-	public static void main(String[] args) throws Exception
-	{
-		Thread3 th1 = new Thread3();
-		th1.start();
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.io.*;
+import java.util.Vector;
 
-		try {
-			Thread.sleep(5*1000);
-		} catch(Exception e) {}
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 
-		throw new Exception("꽝~!!!");
+class Hello extends JFrame {
+
+	public Hello() {
+		setLayout(new BorderLayout());
+		add("South",new JButton("test"));
+		add("Center",new JLabel("label"));
 	}
-}
 
-class Thread3 extends Thread {
-	public void run() {
-		for(int i=0; i < 10; i++) {
-			System.out.println(i);
+	public static void main(String[] args) {
+		Hello hel = new Hello();
+		hel.setTitle("Swing Test");
+		hel.setSize(500, 700);
+		hel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		hel.setVisible(true);
 
-			try {
-				Thread.sleep(1000);
-			} catch(Exception e) {}
-		}
-	} // run()
+	} // main
+
 }
